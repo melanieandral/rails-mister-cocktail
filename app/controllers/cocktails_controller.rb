38f6bcb639f29c5ce3fe013bac3664049fe20e85
Melanie Andral <m.andral@montpellier-bs.com>
2020-05-14 14:54:45 +0200

    Controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,6 +4,7 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -11,5 +12,23 @@ class CocktailsController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+
   end
+
+private
+
+def set_cocktail
+  @cocktail = Cocktail.find(params[:id])
+end
+
+def cocktail_params
+  params.require(:cocktail).permit(:name)
+end
+
 end
