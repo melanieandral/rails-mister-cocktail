ee34669b8d582494f8a4d24c804d7a4f4083195f
Melanie Andral <m.andral@montpellier-bs.com>
2020-05-14 14:26:09 +0200

    exos

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,13 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktails = Cocktail.new
   end
 
   def create
