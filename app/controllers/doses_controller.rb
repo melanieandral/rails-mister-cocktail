2c45f46cc07c5f51f17a28c8244f73d353f833d9
Melanie Andral <m.andral@montpellier-bs.com>
2020-05-15 12:39:12 +0200

    exos

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,14 +6,13 @@ class DosesController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      @review = Review.new
-      render "cocktails/show"
+      render :new
     end
   end
 
@@ -26,6 +25,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
